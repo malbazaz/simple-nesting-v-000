74ce5cebe74c50d0397ee69195d254930b2bf685
Mo Al <malbazaz@mba2018.hbs.edu>
2018-06-15 04:11:51 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -107,7 +107,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-  programmer_hash[:alan_kay][:known_for]="GUI"  
+  programmer_hash[:alan_kay][:known_for]= "GUI"  
+  programmer_hash
      
 end
 
@@ -128,4 +129,5 @@ def adding_to_dennis
         }
      }
 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 end
