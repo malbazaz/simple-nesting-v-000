0a87069927f586604b6acac1e9bbd168f0ac57ef
Mo Al <malbazaz@mba2018.hbs.edu>
2018-06-15 04:07:51 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -77,6 +79,10 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
+        }
+        :yukhiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP","C"]
         }
      }
 
@@ -101,7 +107,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+  programmer_hash[:alan_kay][:known_for]="GUI"  
      
 end
 
@@ -121,5 +127,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 end
